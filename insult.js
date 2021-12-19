d9ed21e4d561c33c8c07cffa8036285b48d90b98
renshoek <58260544+renshoek@users.noreply.github.com>
2021-12-19 17:54:59 +0100

    Add files via upload

---
+++ insult.js
@@ -0,0 +1,129 @@
+function makeInsult(length) {
+    var result = '';
+
+    let words = 'braddyville unmesmeric envapor travally furtiveness yttro- cutpurses pre-linnean purple-headed surpass young-womanishness zap ruddiness dernly purreic schoenius heh mesoplastic lornnesses counterparallel fibrilliform well-lost overfurnishing unhushed snm taglock fausto well-plumed antiharmonist investiture kempas monocondylar albuminoscope scrae acusector askant unretrograding punching catamaran amram unpleasantries hatchetfaced ringless pericementum stocks bookful tab pelecypod bajoire urochs beanweed uncurricularized oranginess magnifically letha clave tantrum trigonia leniences safes infested pyle wanatah textbookless ernesta offensible skillfulness ecophobia volvoxes meanspiritedly all-armed unbeguileful gomavel solate tawny-olive nonemission deer-staiker unuxorious matthiew aggregata mowing fradicin half-playfully wallpapered coagel rationalizing gentil dysmerogenesis foisting palladosammine picine riveter poss non-gothically depriorize imprecatory coasters correligionist histography youthsome thiocarbamide menoetes shutdown unroosting summerliness nonsexually vaunt overphilosophizing bargersville rabbinates lymphectasia cabio draftees thirstless schizognathae hide-out dissipates belitter matchless cafetal overoptimistically detoxifying flesh-consuming mucklucks rotundate boutade thanatophobe elf-shot stonished raines prophetless incorrespondent flashlamp well-stained diaereses osteotome octocoralline sharecroppers opisthographal athelia half-present hurless illusion mall budgeree kundalini subterraneous potto transmissometer undeistical shield-fern middle-aged jettie unconscionably polysiphonous frederiksen tidiable punishments elkwood half-centuries melodramatised aerification jibuti gisella klendusity rauriki teasle aiolis untremolant poultryist anomic jaret unframableness heart-sore imbowered unprivateness shrilled unreliably potcher gunpapers sophiologic emeries reindorsed stoke-hole drearness mastorrhagia honey-stalks ambisextrous hoga one-storied make-shame noneclectic footmenfootpad weaved warworn qeshm himinbjorg pranksomeness manzanita adiaphorite departmentalized friths snippier manks heiling valeriana unconceiving frontiers splintage three-weekly colocynth quarreled many-winding excimers memorialised unconsentient hannayite dhauri politicizing ermey flip-flopped red-spotted angelicize falconnoid proustian calzone unobesely intercepter hydrogels reassuringly tric-trac candlelighted blackamoors white-hooved caraho unsportive room half-armor bacteritic fermerer as curraghs angiocarpic earth-light nonimposition homoplassy slabbers theer belda beefiest kits semisuccessful pegall fomite geometrized tackles crozzly creepmousy smut-free predesignating tiki eloped barrada meeting cablelike golgi gerome sthenic navigates hemorrhoid crossbowman intellectualizing preprandial assertiveness crannoger capstans gallberry hanapers engorgement andreotti undiscoverability nepmen sdlc nyu pre-executor noncontagious tracheotomy meta isotony slippiness cloth-backed spawny enwinds unpanniered spear-billed lincture eye-beam sardiuses viewsome unmolestedly'
+    var insultArray = words.split(' ');
+
+    var maxAmount = 1;
+
+    for (var i = 0; i < maxAmount; i++) {
+        randomNum = Math.floor(Math.random() * insultArray.length);
+        randomNum2 = Math.floor(Math.random() * insultArray.length);
+    }
+
+    const sentence = [   
+        
+"My mom called me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " yesterday... I don't know if thats a good thing",
+
+"I dropped a pan of spaghetti and now my coworkers wont stop calling me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"Some grandma cut me off in traffic and called me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I ate all my mom's donuts and now she keeps calling me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"Yeah man, I legally changed my name to " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " yesterday",
+        
+"I accidently slipped on an onion and now everybody at school calls me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I gave my neighbour the nickname " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ", He's exactly that.",
+
+"I named my starter pokemon " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I found out my grandfather's real name is " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". I am devestated.",
+
+"The governement took my son away because I kept calling him " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"The name of my next project will be called " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() +  ", and yes thats exactly what you think it is.",
+
+"My postman keeps calling me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " and I dont know why",
+
+"The name of this new infection is " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". I think you can already guess what the results are.",
+
+"Our minister of agriculture got fired because he called the prime-minister " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I don't know the title of the song but the lyrics are like... 'yeah you're such a sexy " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " yeah'", 
+
+"Did you see " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " yet? It's a new series on Netflix starring Daniel Craig.",
+
+"Did you know that The Rock's actual name is " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + "?",
+
+"Joe Biden? Oh, you mean " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"Dude, McDonalds named their new burger " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() +  ". That's just cultural appropriation.",
+
+"My dog is actually a different breed called " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I wouldn't take the vaccine. Did you know they put " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " in there?",
+
+"I am " + insultArray[randomNum].bold() + "-" + insultArray[randomNum2].bold() + "-sexual, you bigot.",
+        
+"I wish I named my dog " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() +  ". It just sounds way better than Poopdollah.",
+
+"Instead of presidents they should put " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " on the American dollar bills",
+
+"Please think for a second. You're doing it wrong you dumb, stupid " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"I'll give you a hint; It has something to do with " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + "...",
+
+"One of the many scrapped ideas was 'Harry Potter and the " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + "'",
+        
+"The wifi password is " + insultArray[randomNum].bold() + "-" + insultArray[randomNum2].bold() + ", in case you didn't know it yet.",
+        
+"The title of my new book is " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". That basically sums up my whole life.",
+
+"Actually, the scientific name for guinea pig is " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"The name of my new company will be "  + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". It's a very specific type of business.",
+        
+"I don't know why my teacher keeps calling me "  + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". I think he's making fun of me.",
+        
+"The model after the Samsung S21 will definitly be called Samsung S-"  + insultArray[randomNum].bold() + "-" + insultArray[randomNum2].bold(),
+
+"And on the 8th day God said '"  + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + "', but nobody really knew what he ment with that.",
+        
+"Did you know Canada's motto actually translates to "  + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"You're such a "  + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"You have Batman, Spiderman, Superman... so I came up with "  + insultArray[randomNum].bold() + "-" + insultArray[randomNum2].bold() + "-man, and now I am barred from entering their studio.",
+
+"What the fuck did you just say to me, you little " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+
+"We called him " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " for about a week until he started crying",
+
+"Amogus. Sus. " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". Imposter. Those are all words from the popular hit game Among Us.",
+
+"I graduated in " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " 12 years ago and I have yet to find a job that suits me.",
+        
+"My dad wanted me to become a neurosurgeon, but I majored in " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " instead... I regret that decision now.",
+        
+"Whoever threw that paper; your mom's a " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"" + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " is stealing this election! The media is covering up!",
+    
+"Listen up kid. If you call me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + " one more time I will burn down your skateboard. Got it?",
+        
+"If people were expressed in words, you would definitly be called " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"My wife's boyfriend keeps calling me " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ". Do you think he's making fun of me?",
+        
+"I (32) accidently called my mother-in-law (60) " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold() + ", and now she won't talk to me again. AITA?",
+        
+"TIFU by baptizing my stepson as " + insultArray[randomNum].bold() + " " + insultArray[randomNum2].bold(),
+        
+"You have been permanently banned from r/" + insultArray[randomNum].bold() + "" + insultArray[randomNum2].bold() + ". You can still view and subscribe, but you won't be able to post or comment.",
+            
+];
+    var maxAmount = 1;
+
+    for (var i = 0; i < maxAmount; i++) {
+        randomNum = Math.floor(Math.random() * sentence.length)
+    }
+
+
+    return sentence[randomNum];
+}
+
+document.getElementById("button").addEventListener("click", function () {
+    document.getElementById("babel").innerHTML = makeInsult();
+});;
